06a4f4bb025b5021d2f474f2ce19cb5f1e6ad991
N. Taylor Mullen <nimullen@microsoft.com>
2015-10-20 21:27:22 -0700

    Finished ManageHasPasswordTagHelper.

---
+++ TagHelperDemo/TagHelpers/ManageHasPasswordTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperDemo.TagHelpers
+{
+    [OutputElementHint("a")]
+    public class ManageHasPasswordTagHelper : TagHelper
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public ManageHasPasswordTagHelper(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        public bool Change { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+
+            var linkContent = Change ? "Change" : "Set";
+            output.Content.SetContent(linkContent);
+
+            var actionName = Change ? "ChangePassword" : "SetPassword";
+            output.Attributes["href"] = _urlHelper.Action(actionName, "Manage");
+
+            output.PreElement.SetContent("[ ");
+            output.PostElement.SetContent(" ]");
+
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
